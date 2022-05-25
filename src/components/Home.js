f8ee4e5092898f75e68a869c8b914b7dc2bbc621
yellow-mi <miriam.lent11@gmail.com>
2022-05-25 14:53:01 +0200

    feat: create few components

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Question from './Question'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Question />
+            </div>
+        )
+    }
+}
+
+export default Home
