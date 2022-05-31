ab94851de872fdb08377d10a26ff7fab2e0e215e
yellow-mi <miriam.lent11@gmail.com>
2022-05-31 12:01:49 +0200

    feat: create middlewares

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import Home from './Home';
 import { connect } from 'react-redux'
 import React, { Component } from 'react';
+import { handleInitialData } from '../actions/shared'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  
   render() {
     return (
       <Router>
