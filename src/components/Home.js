b31f5de0324721ed388d8f9fcc45cf6fd2a801b4
yellow-mi <miriam.lent11@gmail.com>
2022-06-09 14:39:50 +0200

    feat: work on home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,34 @@
 import { connect } from "react-redux";
 import Dashboard from "./Dashboard";
 import React, { Component } from "react";
+import Question from "./Question";
 class Home extends Component {
+    state = {
+        showList: 'unanswered'
+    }
+
+    handleList = e => {
+        this.setState((prevState) => ({
+            showList: !prevState.showList 
+        }))
+    }
+    
     render() {
+        const { showList } = this.state
+        const { answered, unanswered } = this.props
+        
         return (
             <div className="center">
                 <h3 className="center">Let's play:</h3>
-                <div>
-                    <button>ANSWERED</button>
-                    <button>UNANSWERED</button>
-                </div>
                 <div className="page">
+                    <ul>
+                        {
+                            showList === 'unanswered' ? unanswered.map(question => <Question key={question.id} />)
+                         : answered.map(question => <Question key={question.id} />)
+                        } 
+                    </ul>
+                    <button onClick={this.handleList}>ANSWERED</button>
+                    <button onClick={this.handleList}>UNANSWERED</button>
                 </div>
                <Dashboard />
             </div>
@@ -18,4 +36,20 @@ class Home extends Component {
     }
 }
 
-export default connect()(Home)
+function mapStateToProps ({ authedUser, questions,users }) {
+    const answeredIds = Object.keys(users[authedUser].answers)
+    const answered = Object.values(questions)
+    .filter(question => answeredIds.includes(question.id))
+    const unanswered = Object.values(questions)
+    .filter(question => !answeredIds.includes(question.id))
+    .sort((a,b) => b.timestamp - a.timestamp)
+    
+    return {
+        userQuestionData: {
+            answered,
+            unanswered
+        }
+    }
+}
+
+export default connect(mapStateToProps)(Home)
